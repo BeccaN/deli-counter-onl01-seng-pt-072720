d20ad1a16bc0ca6701b6dfcba236d97212cae547
Rebecca Congi <rncongi@gmail.com>
2020-08-03 13:45:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def now_serving(katz_deli)
   if katz_deli.length > 0 
     katz_deli.each do |person|
       puts "Currently serving #{person}."
-      
+      katz_deli.shift
     end   
   else
     puts "There is nobody waiting to be served!"
