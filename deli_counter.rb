f0cce368d514f8f0f335882555bb8bb79fc8ae3a
Rebecca Congi <rncongi@gmail.com>
2020-08-03 13:03:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = ["Tyler", "Alex"]
+def line(katz_deli)
+  if katz_deli.length > 0 
+    x = "The line is currently: "
+    katz_deli.each do |person|
+      num = 1
+      x << #{num} + ". " + #{person}
+      num += 1 
+    end
+    puts x 
+  else
+    puts "The line is currently empty."
+  end 
+end   
+
+line(katz_deli)
