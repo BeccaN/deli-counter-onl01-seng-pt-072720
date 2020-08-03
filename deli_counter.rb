f5a2f3378c08a4656419f9eb6e360cf25d01acc2
Rebecca Congi <rncongi@gmail.com>
2020-08-03 13:05:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ katz_deli = ["Tyler", "Alex"]
 def line(katz_deli)
   if katz_deli.length > 0 
     x = "The line is currently: "
+    num = 1
     katz_deli.each do |person|
-      num = 1
-      x << "#{num}" + ". " + "#{person}"
+      x << "#{num}" + " . " + "#{person}"
       num += 1 
     end
     puts x 
